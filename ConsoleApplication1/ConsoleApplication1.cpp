616673c4d73f18e1d022279d2db8c91f6f0da971
Ngô Thế Kiệt <kietsieubeo44@gmail.com>
2024-07-13 19:50:04 +0700

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+int largest_digit(int n) {
+    int a = n / 100;
+    int b = (n / 10) % 10;
+    int c = n % 10;
+
+    int max_n = a;
+    if (b > max_n) max_n = b;
+    if (c > max_n) max_n = c;
+
+    return max_n;
+}
+
+void main() {
+    int n;
+    printf("nhap vao so nguyen duong 3 chu so : ");
+    scanf_s("%d", &n);
+
+    if (n < 100 || n > 999) {
+        printf("khong phai so co 3 chu so");
+    }
+    else {
+        printf("Chu so lon nhat la %d\n", largest_digit(n));
+    }
+}
